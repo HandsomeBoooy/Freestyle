29377c8de1482f8d5cdd0cdbb80707b65f2d2f6f
XingLiu <mustangshelbygt500@163.com>
2018-03-08 18:35:50 +0800

    some additions and deletions
    add resume_block file, and move the stylesheet files into the new resume
    folder

--- SiteService/routes/index.js
+++ SiteService/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: "Welcome to Miata's Home Page." });
+    res.render('index', { title: "Welcome to Xing Liu's Homepage." });
 });
 
 router.get('/resume', (req, res, next) => {
