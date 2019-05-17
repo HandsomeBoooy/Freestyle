f19bd0018468315f4cbd5a0574dabba7fec60839
MiataXing <mustangshelbygt500@163.com>
2019-05-17 13:48:57 +0800

    add a expression of email validate regular

--- src/js/regExp/index.js
+++ src/js/regExp/index.js
@@ -5,7 +5,12 @@ class RegularExpression {
     static UrlMatch(url) {
         let expression = /(ht|f)tp(s?):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/;
         let expObj = new RegExp(expression);
-        let flag = expObj.test(url);
-        return flag;
+        return expObj.test(url);
+    }
+
+    static EmailMatch(email) {
+        let expression = '^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$';
+        let expObj = new RegExp(expression);
+        return expObj.test(email);
     }
 }
